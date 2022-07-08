7ca193ca991627b90f88c710d20b256002645536
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-08 19:36:53 -0400

    Borders

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
 import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from './app/screens/ViewImageScreen';
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
