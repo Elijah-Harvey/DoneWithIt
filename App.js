13356d7d260a799f2c4f40036e3ad60fc6dfcdb6
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-08 11:06:22 -0400

    Platform-specific Code

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View, Alert, Button } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Alert,
+  Button,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={[ styles.container, containerStyle ]}>
-      <Button  title="Click me" onPress={() => console.log("Press me")} />
+    <View style={styles.container}>
+      <Button title="Click me" onPress={() => console.log("Press me")} />
     </View>
   );
 }
-
-const containerStyle = { flex:1 , backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#709",
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : "",
   },
 });
