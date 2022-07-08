fa81c1e9aba432fc9ce709f186a8719c52d3dac2
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-08 19:56:26 -0400

    Paddings and Margins

--- App.js
+++ App.js
@@ -17,8 +17,25 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
+          padding: 20,
+          paddingHorizontal: 10,
+          paddingLeft: 30,
         }}
-      ></View>
+      >
+        <View
+          style={{
+            backgroundColor: "gold",
+            width: 50,
+            height: 50,
+          }}
+        ></View>
+      </View>
+      <View style={{
+        backgroundColor: "tomato",
+        width: 100,
+        height: 100,
+        margin: 20,
+      }}></View>
     </View>
   );
 }
