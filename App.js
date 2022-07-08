97e4f88b1fc234131737ca75d8997d15bf1f32a6
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-08 12:40:14 -0400

    Flexbox- flexBasis, flexGrow and flexShrink

--- App.js
+++ App.js
@@ -11,42 +11,29 @@ export default function App() {
         justifyContent: "center",
         alignItems: "center",
         alignContent: "center",
-        flexWrap: "wrap",
+        // flexWrap: "wrap",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
+          width: 400,
+          flexShrink: -1,
+          height: 100
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
           width: 100,
-          height: 100,
+          height: 100
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
           width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "grey",
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "greenyellow",
-          width: 100,
-          height: 100,
+          height: 100
         }}
       />
     </View>
