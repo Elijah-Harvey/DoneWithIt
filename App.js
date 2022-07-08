b37ed1780dc502fdd8b580fd44661b8f7663e441
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-08 19:49:05 -0400

    Shadows

--- App.js
+++ App.js
@@ -17,9 +17,6 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
-          borderWidth: 10,
-          borderColor: "royalblue",
-          borderRadius: 50,
         }}
       ></View>
     </View>
