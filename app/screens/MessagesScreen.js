8218bda316844b6a18238835e417005b38b70db3
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-09 12:18:59 -0400

    Extracting the Screen Component

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
+
 import ListItem from "../components/ListItem";
+import Screen from "../components/Screen";
 
 const messages = [
   {
@@ -19,17 +21,24 @@ const messages = [
 
 function MessagesScreen(props) {
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(message) => message.id.toString()}
-      renderItem={({ item }) => 
-      <ListItem 
-      title={item.title}
-      subTitle={item.description}
-      image={item.image}
-      />}
-    />
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  
+})
 
 export default MessagesScreen;
