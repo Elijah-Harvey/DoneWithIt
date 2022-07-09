6ef6bd52b6a0a9ebaf81ab629434cfd1133beb5a
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-09 10:55:39 -0400

    FlatList

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from "react";
 import { View } from "react-native";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <MessagesScreen />
   );
 }
