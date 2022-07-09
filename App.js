d2c1f9506504faec97e12bcbbcb9f36adc98151c
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-09 15:37:00 -0400

    Building the Icon Component

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { View } from "react-native";
 import Card from "./app/components/Card";
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
+import Icon from "./app/components/Icon";
+import Screen from "./app/components/Screen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import ListItem from './app/components/ListItem';
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <Screen>
+      <ListItem title="My Title" subTitle="My subtTitle" />
+    </Screen>
   );
 }
