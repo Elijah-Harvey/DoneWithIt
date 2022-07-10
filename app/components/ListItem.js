93504a132a9a1f1ee0ef51cbc1cb2e54fc82b1da
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-10 11:13:28 -0400

    Handling Long Text

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -21,8 +21,8 @@ function ListItem({
           {IconComponent}
           {image && <Image style={styles.image} source={image} />}
           <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
+            <AppText numberOfLines={1} style={styles.title}>{title}</AppText>
+            {subTitle && <AppText style={styles.subTitle} numberOfLines={2}>{subTitle}</AppText>}
           </View>
           <MaterialCommunityIcons
             color={colors.meduim}
