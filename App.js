46352221902c2c60200214a4ef2c042c4fb05094
Elijah-Harvey <potenialtrader@gmail.com>
2022-07-09 20:12:58 -0400

    Switch

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useState} from "react";
-import { Text, TextInput, View } from "react-native";
+import React, { useState } from "react";
+import { Switch, Text, TextInput, View } from "react-native";
 import Card from "./app/components/Card";
 import Icon from "./app/components/Icon";
 import Screen from "./app/components/Screen";
@@ -7,16 +7,16 @@ import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListItem from "./app/components/ListItem";
-import AccountScreen from './app/screens/AccountScreen';
+import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
-import AppTextInput from './app/components/AppTextInput';
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  const [firstName, setFirstName] =useState('')
+  const [isNew, setIsNew] = useState(false);
 
   return (
     <Screen>
-      <AppTextInput placeholder="Username" icon="email"/>
+      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
     </Screen>
   );
 }
